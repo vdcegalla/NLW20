56804f285e5c63dc33661d08ecb3cac7632fc4fc
Vinicius <vinicius.cegalla@gmail.com>
2025-07-17 21:58:43 -0300

    Start implementing website logic with JavaScript

---
+++ script.js
@@ -0,0 +1,6 @@
+const apiKeyInput = document.getElementById('apiKey');
+const gameSelect = document.getElementById('gameSelect');
+const questionInput = document.getElementById('questionInput');
+
+
+console.log(apiKeyInput)
